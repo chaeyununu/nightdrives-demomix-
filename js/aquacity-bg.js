f253d1f0ef5d41da0bb293555a8562dcc74b4758
chaeyun <ipsac30333@gmail.com>
2026-09-10 03:56:29 +0900

    Refactor left wall handling in AquaCityBg
    Removed left wall responsive adjustments and related variables.

--- js/aquacity-bg.js
+++ js/aquacity-bg.js
@@ -32,10 +32,6 @@
   var PROP_ROUTE_Z_B = -68;
   var HEAVY_ROUTE_FIRST = 3;
   var HEAVY_ROUTE_EVERY = 3;
-  var LEFT_WALL_DESKTOP_NUDGE = 0.08;
-  var LEFT_WALL_NARROW_ASPECT_START = 1.1;
-  var LEFT_WALL_NARROW_ASPECT_FULL = 0.65;
-  var LEFT_WALL_NARROW_EXTRA_INSET = 0.5;
 
   var VORONOI = [
     'vec3 hash(vec3 p){',
@@ -208,7 +204,6 @@
     this._floats = [];
     this._sideObjects = [];
     this._poolSegments = [];
-    this._leftWallMeshes = [];
     this._poolPropCache = {};
     this._propSlots = [];
     this._propRouteState = 'intro';
@@ -230,7 +225,6 @@
     this._fpsAvg = 60;
 
     this._buildScene();
-    this._applyResponsiveLeftWall(window.innerWidth / window.innerHeight);
     this._preloadAllProps();
     this._maybePreloadNextHeavy();
 
@@ -241,10 +235,14 @@
   AquaCityBg.prototype._buildScene = function () {
     var self = this;
     var tileMat = makeMat({ color: 0xffffff, roughness: 0.0 });
-    var leftWallMat = makeMat({
+    var leftWallMat = new THREE.MeshBasicMaterial({
       color: 0xffffff,
-      roughness: 0.0,
-      side: THREE.DoubleSide
+      side: THREE.DoubleSide,
+      fog: false,
+      depthWrite: false,
+      polygonOffset: true,
+      polygonOffsetFactor: -8,
+      polygonOffsetUnits: -8
     });
 
     var texLoader = new THREE.TextureLoader();
@@ -257,10 +255,8 @@
       texLoader.load(ASSET_BASE + item[1], function (tex) {
         tex.wrapS = tex.wrapT = THREE.RepeatWrapping;
         tex.repeat.set(SIZE / 4, SIZE / 4);
-        [tileMat, leftWallMat].forEach(function (mat) {
-          mat[item[0]] = tex;
-          mat.needsUpdate = true;
-        });
+        tileMat[item[0]] = tex;
+        tileMat.needsUpdate = true;
       });
     });
 
@@ -279,8 +275,8 @@
         mesh.castShadow = false;
         mesh.receiveShadow = false;
         if (mat === leftWallMat) {
-          mesh.userData.leftWallBaseX = x;
-          self._leftWallMeshes.push(mesh);
+          mesh.position.x += 0.08;
+          mesh.renderOrder = 30;
         }
         group.add(mesh);
       }
@@ -423,20 +419,6 @@
     mesh.position.set(0, 3.05, -24);
     mesh.renderOrder = 2;
     this._sc.add(mesh);
-  };
-
-  AquaCityBg.prototype._getLeftWallInset = function (aspect) {
-    var t = (LEFT_WALL_NARROW_ASPECT_START - aspect) /
-      (LEFT_WALL_NARROW_ASPECT_START - LEFT_WALL_NARROW_ASPECT_FULL);
-    t = Math.max(0, Math.min(1, t));
-    return LEFT_WALL_DESKTOP_NUDGE + t * LEFT_WALL_NARROW_EXTRA_INSET;
-  };
-
-  AquaCityBg.prototype._applyResponsiveLeftWall = function (aspect) {
-    var inset = this._getLeftWallInset(aspect);
-    this._leftWallMeshes.forEach(function (mesh) {
-      mesh.position.x = mesh.userData.leftWallBaseX + inset;
-    });
   };
 
   function deskinRoot(root) {
@@ -933,10 +915,8 @@
   AquaCityBg.prototype._resize = function () {
     var w = window.innerWidth;
     var h = window.innerHeight;
-    var aspect = w / h;
-    this._cam.aspect = aspect;
+    this._cam.aspect = w / h;
     this._cam.updateProjectionMatrix();
-    this._applyResponsiveLeftWall(aspect);
     this._ren.setSize(w, h);
   };
 
